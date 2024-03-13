9530d5abb9e9f64db098fd055348dc3b7826d52a
Semenov <semenov@skeeks.com>
2024-03-13 11:47:13 +0300

    dev

--- src/config/console.php
+++ src/config/console.php
@@ -16,4 +16,10 @@ return [
         '@webroot' => '@frontend/web',
         '@web'     => '',
     ],
+    
+    'components' => [
+        'urlManager' => [
+            'hostInfo' => ''   
+        ]
+    ]
 ];
